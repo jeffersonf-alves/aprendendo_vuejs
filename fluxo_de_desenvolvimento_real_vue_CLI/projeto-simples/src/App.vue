9c2ade415ccbe8e5fee320a3c6850dfd9711ae98
Jefferson Alves <jefff9910@outlook.com>
2022-01-13 09:35:23 -0300

    fluxo de desenvolvimento com vue finalizada

---
+++ fluxo_de_desenvolvimento_real_vue_CLI/projeto-simples/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <h1>{{ titulo }}</h1>
+
+</template>
+
+<script>
+export default  {
+  data: function() {
+    return {
+      titulo: "Ola vuejs"
+    }
+  },
+  created: {
+      change() {
+            setInterval(function() {
+                this.titulo = "Ola Jefferson"
+            }, 1000)
+      }
+  }
+}
+
+</script>
+
+<style scoped>
+
+
+</style>
